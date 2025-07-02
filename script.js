fd432379ff2123c8d402bfd0701de811586e68c5
joão vitor <vitorbotelho0309@gmail.com>
2025-07-01 21:32:09 -0300

    curriculo

---
+++ script.js
@@ -0,0 +1,4 @@
+  document.getElementById('btnObjetivo').addEventListener('click', function() {
+    const objetivo = "O objetivo da aplicação é demonstrar minhas habilidades e experiência de forma simples e clara.";
+    document.getElementById('objetivoTexto').textContent = objetivo;
+  });
